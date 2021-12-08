d5baaa33c3436104b2a47a24a8b7e8b9506c1eab
kacemws <berras.belkacem@gmail.com>
2021-12-08 11:47:38 +0100

    fixing layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,10 +2,10 @@ import { Footer, Header } from "../Components";
 
 export const Layout = ({ children, ...props }) => {
   return (
-    <>
+    <div className="min-h-screen bg-textInverted-300">
       <Header />
-      <main>{children}</main>
+      <main className="w-full min-h-screen">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
