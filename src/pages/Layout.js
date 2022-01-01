721f76d3a3ac1f350b918f3ffcee1a6b5d699426
kacemws <berras.belkacem@gmail.com>
2022-01-01 03:44:24 +0100

    fixed header not displaying after a certain scroll point (100vh)

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,7 +2,7 @@ import { Footer, Header } from "../Components";
 
 export const Layout = ({ children, ...props }) => {
   return (
-    <div className="h-screen bg-neutral-200 flex flex-col">
+    <div className="min-h-screen bg-neutral-200 flex flex-col">
       <Header />
       <main className="flex-1 w-full p-4">{children}</main>
       <Footer />
